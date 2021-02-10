c164d79c6baddd3e626d88c523a769a5d70f60ee
Condog9000 <ctnoda@outlook.com>
2021-02-10 13:46:35 -0800

    changed Vendor_ID and Product_ID to unique values

--- keyboards/handwired/isksuren/config.h
+++ keyboards/handwired/isksuren/config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x6768
+#define PRODUCT_ID      0x9001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Condog9000
 #define PRODUCT         Isksuren
